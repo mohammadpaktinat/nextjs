0246d3b37be46d69c71320ceac0fa14c9dce9c73
mohammad paktinat <mohammad.paktinat.p@gmail.com>
2024-10-14 14:10:00 +0330

    starting project

---
+++ components/Button.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+type ButtonProps = {
+  type: "button" | "submit";
+  title: "string";
+  icon?: "string";
+  varient: "string";
+  full?: boolean;
+};
+
+const Button = ({ type, title, icon, varient, full }: ButtonProps) => {
+  return (
+    <button
+      className={`flex items-center gap-3 rounded-full border ${varient} ${
+        full && "w-full"
+      }`}
+      type={type}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap">{title}</label>
+    </button>
+  );
+};
+
+export default Button;
